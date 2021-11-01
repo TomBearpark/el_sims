a6c9b3a74ba06f776902a1bbd23b15d1a0370b06
TomBearpark <t.bearpark@googlemail.com>
2021-11-01 15:45:24 -0400

    slurm fumblings

--- slurm/slurm.sh
+++ slurm/slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=8        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --cpus-per-task=50        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
